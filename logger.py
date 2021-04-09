78c2830620b724afe0186b1bd6457e06328061d0
MetaphoricalGnat <nathan.markle21@gmail.com>
2021-04-09 09:00:02 -0400

    Basic Logger, no documented code

--- logger.py
+++ logger.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
-import os, time
+from datetime import datetime
+
 
 class Logger:
     def __init__(self):
-        self.log_file_path = "./log/"
+        self._log_file_path = "./log/"
 
     def write(self, message):
-        pass
+        logfile = open(self._log_file_path + "logfile.log", "a")
+        logfile.write("%s: %s\n" % (datetime.utcnow(), message))
