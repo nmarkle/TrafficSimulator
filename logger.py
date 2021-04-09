e0a92638856167f9d8a922a3042d65fa9b80fb3f
MetaphoricalGnat <nathan.markle21@gmail.com>
2021-04-09 10:54:41 -0400

    Inserted methods and comments

--- logger.py
+++ logger.py
@@ -1,12 +1,64 @@
 #!/usr/bin/env python3
 
-from datetime import datetime
+__author__ = "Nathan Markle"
+__created__ = "04-09-2021"
+__editor__ = "Nathan Markle"
+__edited__ = "04-09-2021"
+__rationale___ = "Initial Creation"
+__version__ = "0.0.1"
+__maintainer__ = "Nathan Markle"
+__email__ = "nmarkle@highpoint.edu"
+__status__ = "In development"
 
+from datetime import datetime
+import os
 
 class Logger:
     def __init__(self):
-        self._log_file_path = "./log/"
+        """
+        The ultimate logger for all error messages within the Traffic Simulator.
+
+        Parameters
+        ----------
+            N/A
+
+        Methods
+        -------
+            initialize() : Creates the logging directory and file to store data
+            write()      : Writes the given message to the log file
+        """
+        self._log_file_directory = "./log/"
+        self._log_file_name = "logfile.log"
+        self._log_file_path = self._log_file_directory + self._log_file_name
+
+    def initialize(self):
+        """
+        Creates the logging directory and file to store data.
+
+        Parameters
+        ----------
+            N/A
+        
+        Returns
+        -------
+            N/A
+        """
+        os.mkdir(self._log_file_directory)
+        temp = open(self._log_file_path, "x")
+        temp.close()
 
     def write(self, message):
-        logfile = open(self._log_file_path + "logfile.log", "a")
+        """
+        Writes the given message to the log file
+
+        Parameters
+        ----------
+            message (str) : The message to be written to the log file
+
+        Returns
+        -------
+            N/A
+        """
+        logfile = open(self._log_file_path, "a")
         logfile.write("%s: %s\n" % (datetime.utcnow(), message))
+        logfile.close()
